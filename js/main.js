38742dd10d3970faf03fea3a55bcf06c50ebdb9b
johann219 <147496124+johann219@users.noreply.github.com>
2024-02-28 23:04:45 +0300

    Add workflow plan and TO DO list

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// {
+//   id: ,
+//   url: ,
+//   description: ,
+//   likes: ,
+//   comments:
+// }
+
+// id
+// function to generate random unique number from range
+    // function to generate random number (not unique) from range (1-25)
+    // would need to use closures
+
+// url
+// function to generate random unique number from range (1-25)
+    // function to generate random number (not unique) from range
+    // would need to use closures
+// function to form required string (`photos/${i}.jpg`)
+// range is the same as id range, may implement something similar
+
+// descrtiption
+// array with random strings to get from
+// function to get random array element
+    // function to get random number (not unique) from range
+
+// likes
+// function to get random number (not unique) from range (15-200)
+
+// comments
+// function to generate array of objects (Array.from()) with random length
+    //function to get random number from range (0-30)
+// function to generate comment object (to pass to Array.from())
+    // id
+        // function to generate random unique number
+    // avatar
+        // function to get random number from range (1-6)
+        // function to form required string (`img/avatar-${i}.svg`)
+    // message
+        // array with sentences
+        // function to get random number from range (1-2) (to get sentences quantity)
+        // function to get random unique number from range (1-6) (to get unique sentences for evety comment)
+    // name
+        // array with names
+        // function to get random number from range
+
+
+// TO DO LIST
+
+// Array with random names to choose from
+// Array with random descriptions
+// Array with sentences for comments
+
+
+// function to get random number
+// function to get random unique number
+
+// function to get random number from range
+// function to get random unique number from range
+
+// function to form required url strings
+// function to form required avatar strings
+
+// function to create comment object
+// function to create comment objects array
+
+// function to create photo object
+// function to create photo object array
+
