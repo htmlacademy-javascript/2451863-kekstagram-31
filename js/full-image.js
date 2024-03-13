c4e59c6f8928ca401036fa7007bcdb4713955db2
johann219 <147496124+johann219@users.noreply.github.com>
2024-03-13 23:40:01 +0300

    Add correct display of likes and comments count to big picture

--- js/full-image.js
+++ js/full-image.js
@@ -1,13 +1,18 @@
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureImg = document.querySelector('.big-picture__img');
 
-const onMiniatureClick = (miniature) => {
+const onMiniatureClick = ({id, url, description, likes, comments}) => {
   bigPicture.classList.remove('hidden');
-  bigPictureImg.querySelector('img').src = miniature.url;
 
-  // Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
+  // Адрес изображения url подставьте как src изображения внутри блока .big-picture__img
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
+  // Количество лайков likes подставьте как текстовое содержание элемента .likes-count
+  bigPicture.querySelector('.likes-count').textContent = likes;
   // Количество показанных комментариев подставьте как текстовое содержание элемента .social__comment-shown-count.
+  bigPicture.querySelector('.social__comment-shown-count').textContent = 3;
   // Общее количество комментариев к фотографии comments подставьте как текстовое содержание элемента .social__comment-total-count.
+  bigPicture.querySelector('.social__comment-total-count').textContent = comments.length;
+
+
 };
 
 export {onMiniatureClick};
