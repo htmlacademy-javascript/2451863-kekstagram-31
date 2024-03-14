ae50d5842fac432dfd52109ecddfd25f8fe5f70e
johann219 <147496124+johann219@users.noreply.github.com>
2024-03-14 12:46:40 +0300

    Finish big picture creation process

--- js/full-image.js
+++ js/full-image.js
@@ -1,31 +1,58 @@
+import { isEscapeKey } from "./utils.js";
+
 const SHOWN_COMMENTS_COUNT = 3;
 
 const bigPicture = document.querySelector('.big-picture');
 
-const onMiniatureClick = ({id, url, description, likes, comments}) => {
+const onEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+function closeBigPicture() {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onEscKeydown);
+}
+
+function openBigPicture() {
   bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', onEscKeydown);
+}
 
-  // Адрес изображения url подставьте как src изображения внутри блока .big-picture__img
-  bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
-  // Количество лайков likes подставьте как текстовое содержание элемента .likes-count
-  bigPicture.querySelector('.likes-count').textContent = likes;
-  // Количество показанных комментариев подставьте как текстовое содержание элемента .social__comment-shown-count.
-  bigPicture.querySelector('.social__comment-shown-count').textContent = SHOWN_COMMENTS_COUNT;
-  // Общее количество комментариев к фотографии comments подставьте как текстовое содержание элемента .social__comment-total-count.
-  bigPicture.querySelector('.social__comment-total-count').textContent = comments.length;
-
+const renderPictureComments = (comments) => {
   const commentTemplate = bigPicture.querySelector('.social__comment');
-  bigPicture.querySelectorAll('.social__comment').forEach((elem) => elem.remove());
   const commentSection = bigPicture.querySelector('.social__comments');
+  const commentsShownCount = bigPicture.querySelector('.social__comment-shown-count');
+  const commentsTotalCount = bigPicture.querySelector('.social__comment-total-count');
 
-  for (let i = 1; i <= SHOWN_COMMENTS_COUNT; i++) {
+  bigPicture.querySelectorAll('.social__comment').forEach((elem) => elem.remove());
+
+  commentsShownCount.textContent = (comments.length >= SHOWN_COMMENTS_COUNT) ? SHOWN_COMMENTS_COUNT : comments.length;
+  commentsTotalCount.textContent = comments.length;
+
+  for (let i = 0; i < Number(commentsShownCount.textContent); i++) {
     const comment = commentTemplate.cloneNode(true);
-    comment.id = comments[i].id;
+    //comment.id = comments[i].id;
     comment.querySelector('.social__picture').src = comments[i].avatar;
     comment.querySelector('.social__text').textContent = comments[i].message;
     commentSection.appendChild(comment);
-    console.log('Comment Added');
   }
 };
+
+const renderPictureInformation = ({url, description, likes, comments}) => {
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.social__caption').textContent = description;
+  renderPictureComments(comments);
+};
+
+const onMiniatureClick = (miniature) => {
+  renderPictureInformation(miniature);
+  openBigPicture();
+};
+
+bigPicture.querySelector('.big-picture__cancel').addEventListener('click', closeBigPicture);
 
 export {onMiniatureClick};
