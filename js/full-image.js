906b273fde4e5df1664fe13f1bed303fcd3a6550
johann219 <147496124+johann219@users.noreply.github.com>
2024-03-15 13:53:53 +0300

    Refactor code generating comments

--- js/full-image.js
+++ js/full-image.js
@@ -1,6 +1,6 @@
 import { isEscapeKey } from './utils.js';
 
-const MIN_SHOWN_COMMENTS_COUNT = 3;
+const MIN_SHOWN_COMMENTS_COUNT = 5;
 
 const bigPicture = document.querySelector('.big-picture');
 const body = document.querySelector('body');
@@ -27,27 +27,35 @@ function openBigPicture() {
   bigPictureCancel.addEventListener('click', closeBigPicture);
 }
 
+const renderBigPictureComment = (commentInfo, commentTemplate, targetSection) => {
+  const comment = commentTemplate.cloneNode(true);
+  comment.id = commentInfo.id;
+  comment.querySelector('.social__picture').src = commentInfo.avatar;
+  comment.querySelector('.social__text').textContent = commentInfo.message;
+  targetSection.appendChild(comment);
+};
+
 const createBigPictureComments = (comments) => {
-  const commentTemplate = bigPicture.querySelector('.social__comment');
+  const commentTemplate = bigPicture.querySelector('#comment').content.querySelector('li');
   const commentSection = bigPicture.querySelector('.social__comments');
-  const commentsShownCount = bigPicture.querySelector('.social__comment-shown-count');
-  const commentsTotalCount = bigPicture.querySelector('.social__comment-total-count');
+
+  const commentsShownCountDisplay = bigPicture.querySelector('.social__comment-shown-count');
+  const commentsTotalCountDisplay = bigPicture.querySelector('.social__comment-total-count');
 
   bigPicture.querySelectorAll('.social__comment').forEach((elem) => elem.remove());
 
-  commentsShownCount.textContent = (comments.length >= MIN_SHOWN_COMMENTS_COUNT) ? MIN_SHOWN_COMMENTS_COUNT : comments.length;
-  commentsTotalCount.textContent = comments.length;
+  const commentsTotalCount = comments.length;
+  commentsTotalCountDisplay.textContent = commentsTotalCount;
 
-  for (let i = 0; i < Number(commentsShownCount.textContent); i++) {
-    const comment = commentTemplate.cloneNode(true);
-    //comment.id = comments[i].id;
-    comment.querySelector('.social__picture').src = comments[i].avatar;
-    comment.querySelector('.social__text').textContent = comments[i].message;
-    commentSection.appendChild(comment);
+  let commentsShownCount = (commentsTotalCount >= MIN_SHOWN_COMMENTS_COUNT) ? MIN_SHOWN_COMMENTS_COUNT : commentsTotalCount;
+  commentsShownCountDisplay.textContent = commentsShownCount;
+
+  for (let i = 0; i < commentsShownCount; i++) {
+    renderBigPictureComment(comments[i], commentTemplate, commentSection);
   }
 };
 
-const createBigPictureInformation = ({url, description, likes, comments}) => {
+const createBigPictureInformation = ({url, description, likes}) => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.social__caption').textContent = description;
