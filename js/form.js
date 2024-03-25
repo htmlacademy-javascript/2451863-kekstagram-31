84700592f92b536b6d39fe74931f9ad793dfbf1a
johann219 <147496124+johann219@users.noreply.github.com>
2024-03-25 13:49:00 +0300

    Add functionality to close upload overlay on esc keydown

--- js/form.js
+++ js/form.js
@@ -1,17 +1,30 @@
+import { isEscapeKey } from './utils.js';
+
 const uploadFormElement = document.querySelector('.img-upload__form');
 const uploadOverlayElement = uploadFormElement.querySelector('.img-upload__overlay');
 const uploadInputElement = uploadFormElement.querySelector('.img-upload__input');
 const uploadCancelElement = uploadFormElement.querySelector('.img-upload__cancel');
 
-const openUploadOverlay = () => {
-  uploadOverlayElement.classList.remove('hidden');
-  document.body.classList.add('modal-open');
+const onEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadOverlay();
+  }
 };
 
-const closeUploadOverlay = () => {
+function openUploadOverlay () {
+  uploadOverlayElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onEscKeydown);
+}
+
+function closeUploadOverlay () {
   uploadOverlayElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
-};
+
+  document.removeEventListener('keydown', onEscKeydown);
+}
 
 uploadInputElement.addEventListener('change', openUploadOverlay);
 uploadCancelElement.addEventListener('click', closeUploadOverlay);
