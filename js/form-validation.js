c2bc71fd50cb51036ef8b94f83d8d15a2372b9ba
Ivan Komarov <147496124+johann219@users.noreply.github.com>
2024-04-04 13:27:08 +0300

    Fix validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -25,9 +25,14 @@ const checkUniqueHashtag = (hashtagsList) => hashtagsList.every((value, index, a
 const checkHashtagCount = (hashtagsList) => hashtagsList.length <= HASHTAGS_COUNT_MAX;
 
 const validateHashtags = () => {
-  const hashtagsList = uploadHashtagsInput.value.trim().split(' ').map((hashtag) => hashtag.toLowerCase());
+  const hashtagsList = uploadHashtagsInput
+    .value
+    .trim()
+    .split(' ')
+    .map((hashtag) => hashtag.toLowerCase())
+    .filter((hashtag) => hashtag);
 
-  return checkIndividualHashtag(hashtagsList) && checkUniqueHashtag(hashtagsList) && checkHashtagCount(hashtagsList);
+  return hashtagsList.length === 0 || checkIndividualHashtag(hashtagsList) && checkUniqueHashtag(hashtagsList) && checkHashtagCount(hashtagsList);
 };
 
 const getHashtagValidationErrorMessage = () => {
